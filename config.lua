0eec5b71cf2e85021ea4870fbe85fd7a58cd85d0
EatGno <152440715+EatGno@users.noreply.github.com>
2024-12-26 07:57:17 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config = {}
+Config.Locale = 'fr'  -- Changez la langue si nécessaire
+
+-- Liste des modèles pour lesquels appliquer la tête
+Config.PedModels = {
+    's_m_y_sheriff_01',  -- Modèle policier
+    's_m_y_cop_01',      -- Autre modèle
+}
